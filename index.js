b94dba50841422fadeaa10ed45db6ce0d12554f6
BoTolv <47107472+BoTolv@users.noreply.github.com>
2019-05-27 14:57:16 +0200

    igen

--- index.js
+++ index.js
@@ -3,3 +3,6 @@ var target = '_blank';
 var options = "location = yes"
 var ref = cordova.InAppBrowser.open(url, target, options);
 }
+function onDeviceReady() {
+         var ref = window.open('http://apache.org', '_blank', 'location=yes');
+    }
