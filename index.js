afdfee2aef4cb4dc25bc9afff931114815692079
BoTolv <47107472+BoTolv@users.noreply.github.com>
2019-05-27 15:07:21 +0200

    puttede den rigtige kode ind og slettede det andet

--- index.js
+++
@@ -1,8 +0,0 @@
-function openBrowser(url){
-var target = '_blank';
-var options = "location = yes"
-var ref = cordova.InAppBrowser.open(url, target, options);
-}
-function onDeviceReady() {
-         var ref = window.open('http://apache.org', '_blank', 'location=yes');
-    }
